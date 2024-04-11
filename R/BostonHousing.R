6e7ba7c381ce4215afd0770defbb255281c325a8
Shin, Jungmin <c16267@gmail.com>
2024-04-11 09:57:48 +0900

    test

---
+++ R/BostonHousing.R
@@ -0,0 +1,29 @@
+#' Boston house price data
+#' The Boston house-price data of Harrison, D. and Rubinfeld, D.L.
+#' 'Hedonic prices and the demand for clean air', J. Environ. Economics & Management, vol.5, 81-102, 1978.
+#' Used in Belsley, Kuh & Welsch, 'Regression diagnostics, Wiley, 1980.
+#' N.B. Various transformations are used in the table on pages 244-261 of the latter.
+#'
+#' \itemize{
+#'   \item crim     per capita crime rate by town
+#'   \item zn       proportion of residential land zoned for lots over 25,000 sq.ft.
+#'   \item indus    proportion of non-retail business acres per town
+#'   \item chas     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+#'   \item nox      nitric oxides concentration (parts per 10 million)
+#'   \item rm     average number of rooms per dwelling
+#'   \item age      proportion of owner-occupied units built prior to 1940
+#'   \item dis      weighted distances to five Boston employment centres
+#'   \item rad      index of accessibility to radial highways
+#'   \item tax      full-value property-tax rate per $10,000
+#'   \item ptratio  pupil-teacher ratio by town
+#'   \item b        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+#'   \item stat    percent lower status of the population
+#'   \item mdev     Median value of owner-occupied homes in $1000's
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name BostonHousing
+#' @usage data(BostonHousing)
+#' @format A data frame with 506 rows and 14 variables
+NULL
